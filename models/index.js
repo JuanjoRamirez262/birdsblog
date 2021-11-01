405b3a2f027e0b2742e975e9f9d77c06b65baf9e
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-11-01 12:09:32 -0700

    seeds, models skeleton

--- models/index.js
+++ models/index.js
@@ -2,5 +2,15 @@ const User = require('./User')
 const Post = require('./Post')
 const Hike = require('./Hike')
 const Bird = require('./Bird')
+const Comment = require('./Comment')
 
-module.exports = { User, Post, Hike, Bird }
+User.hasMany(Post)
+
+Post.belongsTo(User)
+
+Post.hasMany(Comments)
+Post.hasMany(Birds)
+Post.hasOne(Map)
+Post.hasMany(Likes)
+
+module.exports = { User, Post, Hike, Bird, Comment}
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,13 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Post, Location, Bird, Comment } = require('../models');
 
 const userData = require('./userData.json');
+const postData = require('./postData.json');
+const birdData = require('./birdData.json');
+const locationData = require('./locationData.json');
+const commentData = require('./commentData.json');
+const likeData = require('./likeData.json');
+
 
 const seedDatabase = async () => {
     //add logic
