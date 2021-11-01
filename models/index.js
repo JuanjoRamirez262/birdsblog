ba17ebdcacd896e69b6f2307af8bb419f857cf0e
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-11-01 13:20:15 -0700

    login

--- models/index.js
+++ models/index.js
@@ -1,17 +1,18 @@
 const User = require('./User')
-const Post = require('./Post')
-const Location = require('./Location')
-const Bird = require('./Bird')
-const Comment = require('./Comment')
-const Like = require('./Like')
+// const Post = require('./Post')
+// const Location = require('./Location')
+// const Bird = require('./Bird')
+// const Comment = require('./Comment')
+// const Like = require('./Like')
 
-User.hasMany(Post)
+// User.hasMany(Post)
 
-Post.belongsTo(User)
+// Post.belongsTo(User)
 
-Post.hasMany(Comment)
-Post.hasMany(Bird)
-Post.hasOne(Location)
-Post.hasMany(Like)
+// Post.hasMany(Comment)
+// Post.hasMany(Bird)
+// Post.hasOne(Location)
+// Post.hasMany(Like)
 
-module.exports = { User, Post, Location, Bird, Comment }
+module.exports = User;
+// module.exports = { User, Post, Location, Bird, Comment }
