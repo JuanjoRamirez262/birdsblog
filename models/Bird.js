117523bf7e6791d9afd113964207be5a83407758
cpysden-coder <chris_pysden@mac.com>
2021-11-02 12:48:20 -0700

    working create user, posts and get users, posts

--- models/Bird.js
+++ models/Bird.js
@@ -4,6 +4,30 @@ const sequelize = require('../config/connection');
 
 class Bird extends Model {}
 
-Bird.init()
+Bird.init(
+    {
+    // add properites here, ex:
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    bird_name: {
+        type: DataTypes.STRING,
+    },
+    date_last_seen: {
+        type: DataTypes.STRING,
+    },
+    date_last_location: {
+        type: DataTypes.STRING,
+    }
+},{
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'bird',
+});
 
 module.exports = Bird;
--- models/index.js
+++ models/index.js
@@ -1,18 +1,22 @@
 const User = require('./User')
-// const Post = require('./Post')
+const Post = require('./Post')
 // const Location = require('./Location')
-// const Bird = require('./Bird')
+const Bird = require('./Bird')
 // const Comment = require('./Comment')
 // const Like = require('./Like')
 
-// User.hasMany(Post)
-
-// Post.belongsTo(User)
+User.hasMany(Post, {
+    foreignKey:"user_id",
+    onDelete:"CASCADE"
+})
+Post.belongsTo(User, {
+    foreignKey:"user_id",
+})
 
 // Post.hasMany(Comment)
 // Post.hasMany(Bird)
 // Post.hasOne(Location)
 // Post.hasMany(Like)
 
-module.exports = { User };
+module.exports = { User, Post, Bird };
 // module.exports = { User, Post, Location, Bird, Comment }
