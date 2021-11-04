598d5b31293436ca0e0a1989e98d1ed4751de7a7
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-11-04 13:53:05 -0700

    pull

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,56 +3,6 @@ const router = express.Router();
 const { Post, User, SeenBird } = require("../../models");
 const moment = require('moment')
 
-// router.get("/", (req, res) => {
-//     Post.findAll({
-//         include: [{
-//             model: User,
-//             attributes: {
-//             exclude: ["password"]
-//         }
-//         }]
-//     }).then(dbPosts => {
-//         if (dbPosts.length) {
-//             const posts=dbPosts.map((project)=> project.get({plain: true}));
-
-//             res.render('homepage', {
-//                 posts,
-//             })
-//         } else {
-//             res.status(404).json({ message: "No posts found in db" })
-//         }
-//     }).catch(err => {
-//         console.log(err)
-//         res.status(500).json({ message: "An error occured getting all posts", err: err })
-//     });
-// });
-
-// router.get("/:id", (req, res) => {
-//     User.findAll({
-//       where: {
-//         id: req.params.id
-//       },
-//       attributes: {
-//         exclude: ["password"]
-//       },
-//       include: [{
-//         model: Post,
-//         // attributes: {
-//         //     exclude: ["createdAt", "updatedAt", "password"]
-//         // }
-//       }]
-//     }).then(dbUser => {
-//       if (dbUser.length) {
-//         res.json(dbUser)
-//       } else {
-//         res.status(404).json({ message: "No users found in db" })
-//       }
-//     }).catch(err => {
-//       console.log(err)
-//       res.status(500).json({ message: "An error occured", err: err })
-//     });
-//   });
-
 //create post
 router.post("/", (req, res) => {
     console.log(req.body)
@@ -93,6 +43,16 @@ router.delete("/:id", (req, res) => {
     }).then(delPost => {
         res.json(delPost)
     })
+})
+
+router.get('/', (req,res) => {
+    Post.findAll({
+        where: {
+          user_id: req.session.user_id
+        },
+        include: [{
+          model: SeenBird,
+        }]
 })
 
 module.exports = router;
