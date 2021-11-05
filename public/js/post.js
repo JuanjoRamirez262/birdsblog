08d00d156f64bd564b74a00679296d37ac7dacef
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-11-04 21:09:04 -0700

    post can be created

--- public/js/post.js
+++ public/js/post.js
@@ -1,30 +1,54 @@
 //Create Post from Profile page
-console.log("hello")
 document.querySelector("#post-input").addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    const fetchObj = {
-      description: document.querySelector("#post-description").value,
-    //   password: document.querySelector("#signup-password").value,
-    //   name: document.querySelector("#signup-name").value,
-    
+  evt.preventDefault();
+  console.log(document.querySelector("#bird-selection").value)
+  let bird, location;
+  switch(document.querySelector("#bird-selection").value){
+    case "American Robin":
+      bird = 1;
+      break;
+    case "Eagle":
+      bird = 2;
+      break;
+    case "Condor":
+      bird = 3;
+      break;
+  }
+  switch(document.querySelector("#location-selection").value){
+    case "Mount Rainier National Park":
+      location = 1;
+      break;
+    case "North Cascade":
+      location = 2;
+      break;
+    case "Cheryy Creek Falls":
+      location = 3;
+      break;
+  }
+
+  const fetchObj = {
+    description: document.querySelector("#post-description").value,
+    bird_id: bird,
+    location_id: location,
+
+  }
+  console.log(fetchObj)
+  fetch("/api/posts", {
+    method: "POST",
+    body: JSON.stringify(fetchObj),
+    headers: {
+      "Content-Type": "application/json"
     }
-    console.log(fetchObj)
-    fetch("/api/posts", {
-      method: "POST",
-      body: JSON.stringify(fetchObj),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).then(res => {
-      if (!res.ok) {
-        console.log(res);
-        return alert("trumpet sound")
-      } else {
-        res.json()
+  }).then(res => {
+    if (!res.ok) {
+      console.log(res);
+      res.redirected('/')
+    } else {
+      res.json()
         .then(data => {
           console.log(data)
-            // location.href = `/profile/${data.id}`
+          // location.href = `/profile/${data.id}`
         })
-      }
-    })
+    }
   })
+})
